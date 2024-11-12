24df9a797b5b55772232dee2e2a23b54d604a2fe
pietrossantos <=>
2024-11-11 23:22:49 -0300

    :tada: Commit Initial

---
+++ testes.js
@@ -0,0 +1,69 @@
+// DESAFIO 1
+
+// alert('boas vindas ao nosso site!');
+
+// let nome = 'Lua';
+
+// let idade = 25;
+
+// let numeroDeVendas = 50;
+
+// let saldoDisponivel = 1000;
+
+// alert('Erro! Preencha todos os campos');
+
+// const mensagemDeErro =  'Erro! Preencha todos os campos';
+
+// alert (mensagemDeErro);
+
+// nome = prompt('Qual é o seu nome?');
+
+// idade = prompt('Qual é a sua idade?');
+
+// if(idade >= 18){
+//     alert('Pode tirar a habilitação!')
+// }
+
+// else if(idade <18){
+// alert('Você não pode tirar sua habilitação!')
+// }
+
+
+// DESAFIO 2
+
+// const diaDaSemana = prompt("Qual é o dia da semana?");
+
+// if (
+//   diaDaSemana ==
+//   ("sábado" || "domingo" || "Sábado" || "Domingo" || "sabado" || "Sabado")
+// ) {
+//   alert("Bom final de semana! :)");
+// } else {
+//   alert("Boa semana! :)");
+// }
+
+// const número = prompt("digite um número");
+
+// if (número >= 0) {
+//   alert("esse número é positivo (+)");
+// } else {
+//   alert("seu número é negativo (-)");
+// }
+
+// const pontuação = prompt("digite sua pontuação!");
+
+// if (pontuação >= 100) {
+//   alert("você venceu! :)");
+// }
+
+// else{
+//     alert ('você perdeu! :(');
+// }
+
+// const saldoDoCliente = 'R$ 3.000';
+
+// alert(`Você tem de saldo: ${saldoDoCliente}`);
+
+// const nomeDeUsuário = prompt('Digite seu nome:');
+
+// alert(`Boas Vindas ${nomeDeUsuário}! :)`);
